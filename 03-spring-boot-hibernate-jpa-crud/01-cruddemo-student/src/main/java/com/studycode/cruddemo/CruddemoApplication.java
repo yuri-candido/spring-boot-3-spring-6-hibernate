51250dd452725d7fe8b7c0ac773d0be632b2afd8
Yuri Gomes <yuri.gcandido@hotmail.com>
2024-11-22 14:25:15 -0300

    jpa crud

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/studycode/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/studycode/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -18,12 +20,102 @@ public class CruddemoApplication {
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			createStudent(studentDAO);
+			// createStudent(studentDAO);
 
-			createMultipleStudents(studentDAO);
+			// createMultipleStudents(studentDAO);
+
+			readStudent(studentDAO);
+
+			// queryForStudents(studentDAO);
+
+			// queryForStudentsByLastName(studentDAO);
+
+			// updateStudent(studentDAO);
+
+			// deleteStudent(studentDAO);
+
+			// deleteAllStudents(studentDAO);
 
 		};
 	}
+
+	private void deleteAllStudents(StudentDAO studentDAO) {
+
+		System.out.println("Deleting all students");
+		int numRowsDeleted = studentDAO.deleteAll();
+		System.out.println("Deleted row count: " + numRowsDeleted);
+	}
+
+	private void deleteStudent(StudentDAO studentDAO) {
+
+		int studentId = 3;
+		System.out.println("Deleting student id: " + studentId);
+		studentDAO.delete(studentId);
+	}
+
+	private void updateStudent(StudentDAO studentDAO) {
+
+		// retrieve student based on the id: primary key
+		int studentId = 1;
+		System.out.println("Getting student with id: " + studentId);
+		Student myStudent = studentDAO.findById(studentId);
+
+		// change first name to "John"
+		System.out.println("Updating student ...");
+		myStudent.setFirstName("John");
+
+		// update the student
+		studentDAO.update(myStudent);
+
+		// display the updated student
+		System.out.println("Updated student: " + myStudent);
+	}
+
+	private void queryForStudentsByLastName(StudentDAO studentDAO) {
+
+		// get a list of students
+		List<Student> theStudents = studentDAO.findByLastName("Doe");
+
+		// display list of students
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+
+		// get a list of students
+		List<Student> theStudents = studentDAO.findAll();
+
+		// display list of students
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
+	}
+
+	private void readStudent(StudentDAO studentDAO) {
+
+		// create  a student object
+		System.out.println("Creating new student object ...");
+		Student tempStudent = new Student("Daffy", "Duck", "daffy@luv2code.com");
+
+		// save the student
+		 System.out.println("Saving the student ...");
+		 studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		 int theId = tempStudent.getId();
+		 System.out.println("Saved student. Generated id: " + theId);
+
+		// retrieve student based on the id: primary key
+		 System.out.println("Retrieving student with id: " + theId);
+		 Student myStudent = studentDAO.findById(theId);
+
+		// display student
+		 System.out.println("Found the student: " + myStudent);
+
+	}
+
 
 	private void createMultipleStudents(StudentDAO studentDAO) {
 
--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/studycode/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/studycode/cruddemo/dao/StudentDAO.java
@@ -2,7 +2,21 @@ package com.studycode.cruddemo.dao;
 
 import com.studycode.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update(Student theStudent);
+
+    void delete(Integer id);
+
+    int deleteAll();
 }
